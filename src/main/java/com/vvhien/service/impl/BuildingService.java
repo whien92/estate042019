d981980dda7bb3a37c9110c16e53e651a0762bb6
HienVu <whien92@gmail.com>
2019-07-09 08:47:12 +0700

    ...

--- src/main/java/com/vvhien/service/impl/BuildingService.java
+++ src/main/java/com/vvhien/service/impl/BuildingService.java
@@ -1,10 +1,19 @@
 package com.vvhien.service.impl;
 
+import java.awt.event.ItemEvent;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.vvhien.builder.BuildingSearchBuilder;
 import com.vvhien.converter.BuildingConverter;
@@ -17,6 +26,7 @@ import com.vvhien.service.IBuildingService;
 
 public class BuildingService implements IBuildingService{
 	private IBuildingRepository buildingRepository;
+	private BuildingConverter buildingConverter;
 	
 	/*public static BuildingService getInstance() {
 		return new BuildingService();
@@ -24,6 +34,7 @@ public class BuildingService implements IBuildingService{
 	
 	public BuildingService() {
 		buildingRepository = new BuildingRepository();
+		buildingConverter = new BuildingConverter();
 	}
 	@Override
 	public BuildingDTO save(BuildingDTO buildingDTO) {
@@ -77,9 +88,58 @@ public class BuildingService implements IBuildingService{
 	}
 	@Override
 	public List<BuildingDTO> findAll(BuildingSearchBuilder builder, Pageble pageble) {
-		Map<String, Object> mapSearch = buildMapSearch();
+		Map<String, Object> properties = buildMapSearch(builder);
+		List<BuildingEntity> buildingEntities =  buildingRepository.findAll(properties, pageble);
+		List<BuildingDTO> results = buildingEntities.stream()
+				.map(item -> buildingConverter.convertToDTO(item)).collect(Collectors.toList());
+		return results;
+	}
+	
+	
+	private Map<String, Object> buildMapSearch(BuildingSearchBuilder builder) {
+		Map<String, Object> result = new HashMap<>();
+		
+		try {
+			Field[] fields = BuildingSearchBuilder.class.getDeclaredFields();
+			for(Field field : fields) {
+				if(!field.getName().equals("buildingTypes") && 
+					!field.getName().startsWith("costRent") && 
+					!field.getName().startsWith("areaRent")) {
+					 field.setAccessible(true);
+					 if(field.get(builder) != null) {
+						 result.put(field.getName().toLowerCase(), field.get(builder));
+					 }
+				}
+			}	
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		
+		return result ;
+	}
+	/*private Object getValue(Field field, BuildingSearchBuilder builder) {
+		Object result = null;
+		Method getter = getGetter(field, builder);
+		if(getter != null) {
+			try {
+				result = getter.invoke(builder);
+			} catch (IllegalAccessException | InvocationTargetException | IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			} 
+		}
 		return null;
 	}
+	private Method getGetter(Field field, BuildingSearchBuilder builder) {
+		String getterMethod = "get" + StringUtils.capitalize(field.getName());
+		try {
+			return builder.getClass().getMethod(getterMethod);
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			return null;
+		} 
+	}*/
 
 
 
